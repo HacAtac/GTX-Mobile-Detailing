2acd92f66be2ec1ddef80242cf629b045a7de99d
HacAtac <jhack00@icloud.com>
2022-01-11 22:01:04 -0600

    add reactapp. started nav component

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.port || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
