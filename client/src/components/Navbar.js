86a403707c733996b66c1ad124157c71aa3862db
HacAtac <jhack00@icloud.com>
2022-01-11 22:16:33 -0600

    created nav component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,36 @@ import React from "react";
 function Navbar() {
   return (
     <div>
-      <h1>Navbar Component</h1>
+      <nav className="navbar navbar-expand-lg">
+        <a className="navbar-brand" href="/">
+          GTX-Mobile-Detailing
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="/register">
+                Register
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/login">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
