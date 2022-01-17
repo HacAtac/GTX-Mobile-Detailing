8e5caa3c8f3177bca3507a0fd15c2b35e46e4bb8
HacAtac <jhack00@icloud.com>
2022-01-17 15:10:38 -0600

    have implemented the Delete service by _id (working just fine)

--- client/src/screens/UpdateService.js
+++ client/src/screens/UpdateService.js
@@ -14,16 +14,45 @@ function UpdateService() {
   const [largePrice, setLargePrice] = useState("");
 
   const { loading, error, data } = useQuery(QUERY_SERVICES);
+  // const [updateService] = useMutation(UPDATE_SERVICE, {
+  //   variables: {
+  //     _id: "_id",
+  //     name,
+  //     description,
+  //     imageUrls,
+  //     individualPrice: parseInt(individualPrice),
+  //     smallPrice: parseInt(smallPrice),
+  //     mediumPrice: parseInt(mediumPrice),
+  //     largePrice: parseInt(largePrice),
+  //   },
+  // });
+  // useMutation for UPDATE_SERVICE that only  updates the data that is being passed in from the form
   const [updateService] = useMutation(UPDATE_SERVICE, {
     variables: {
       _id: "_id",
       name,
       description,
       imageUrls,
-      individualPrice,
-      smallPrice,
-      mediumPrice,
-      largePrice,
+      individualPrice: parseInt(individualPrice),
+      smallPrice: parseInt(smallPrice),
+      mediumPrice: parseInt(mediumPrice),
+      largePrice: parseInt(largePrice),
+    },
+    update(cache, { data: { updateService } }) {
+      //update is a function that takes in the cache and the data that is being returned from the mutation and updates the cache with the new data from the mutation
+      console.log(cache);
+      //readQuery is a function that takes in the query and returns the data from the cache
+      const { services } = cache.readQuery({ query: QUERY_SERVICES });
+      cache.writeQuery({
+        query: QUERY_SERVICES,
+        data: {
+          services: services.map(
+            (
+              service //were mapping through the services and returning the new data that is being returned from the mutation
+            ) => (service._id === updateService._id ? updateService : service) //this says if the service._id is equal to the updateService._id then updateService is returned, otherwise the service is returned so that the cache is updated with the new data
+          ),
+        },
+      });
     },
   });
 
@@ -60,28 +89,28 @@ function UpdateService() {
           <h3 className="card-title">Individual Price</h3>
           <input
             className="input-field"
-            type="text"
+            type="integer"
             value={individualPrice}
             onChange={(e) => setIndividualPrice(e.target.value)}
           />
           <h3 className="card-title">Small Price</h3>{" "}
           <input
             className="input-field"
-            type="text"
+            type="integer"
             value={smallPrice}
             onChange={(e) => setSmallPrice(e.target.value)}
           />
           <h3 className="card-title">Medium Price</h3>{" "}
           <input
             className="input-field"
-            type="text"
+            type="integer"
             value={mediumPrice}
             onChange={(e) => setMediumPrice(e.target.value)}
           />
           <h3 className="card-title">Large Price</h3>{" "}
           <input
             className="input-field"
-            type="text"
+            type="integer"
             value={largePrice}
             onChange={(e) => setLargePrice(e.target.value)}
           />
