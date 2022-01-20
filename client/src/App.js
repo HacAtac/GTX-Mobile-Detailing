4c0fe2aa48974ab8e88577eb8d127988411b9bd8
HacAtac <jhack00@icloud.com>
2022-01-20 06:36:16 -0600

    hmm

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ import Booking from "./screens/Booking";
 
 //Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
+  uri: "https://gtxdetailing.herokuapp.com/graphql",
 });
 // const httpLink = createHttpLink({
 //   uri: "/graphql",
